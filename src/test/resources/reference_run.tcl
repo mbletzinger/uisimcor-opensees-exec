63ad533152753e573d9db5fefd47a0f9c763bfad
mbletzin <mbletzin@d001af02-91e2-447c-bc6e-fea64fa78a0a>
2013-05-09 14:50:14 +0000

    Fixed RZ for 2D simulations

--- src/test/resources/reference_run.tcl
+++ src/test/resources/reference_run.tcl
@@ -4,7 +4,7 @@ recorder Node -file tmp_disp.out -node -dof 1 2 3 disp
 recorder Node -file tmp_forc.out -node -dof 1 2 3 reaction
 pattern Plain 99003	Constant {
 sp 2 1 130.20300000000000000E-009
-sp 2 6 34.000120000000000000E-012
+sp 2 3 34.000120000000000000E-012
 sp 3 1 120.03450000000000000E-009
 sp 4 1 150.11000000000000000E-009
 
