38f57008c816f8db77453de0a81acae2d5e7ed3b
mbletzin <mbletzin@d001af02-91e2-447c-bc6e-fea64fa78a0a>
2013-06-28 15:50:28 +0000

    Added static analysis back in from rev 106 for stiffness evaluations.

--- branches/UiSimCorUpgrade/fem_execute/src/test/resources/config/run_example.tcl
+++ branches/UiSimCorUpgrade/fem_execute/src/test/resources/config/run_example.tcl
@@ -1,10 +1,14 @@
-source LeftCol.tcl
+source Middle.tcl
 source StaticAnalysisEnv.tcl
-recorder Node -file tmp_disp.out -node -dof 1 2 3 disp
-recorder Node -file tmp_forc.out -node -dof 1 2 3 reaction
-pattern Plain 999999	Constant {
-sp 1 1 00.002300000e-14
-sp 3 1 00.000001200e-14
+
+recorder Node -file tmp_disp.out -node 2 3 4 -dof 1 2 3 disp
+recorder Node -file tmp_forc.out -node 2 3 4 -dof 1 2 3 reaction
+pattern Plain 99003	Constant {
+sp 2 1 130.20300000000000000E-009
+sp 2 3 34.000120000000000000E-012
+sp 3 1 120.03450000000000000E-009
+sp 4 1 150.11000000000000000E-009
+
 }
 analyze 1
-remove loadPattern 999999
+remove loadPattern 99003
--- branches/UiSimCorUpgrade/fem_execute/src/test/resources/reference_init.tcl
+++
@@ -1,4 +0,0 @@
-source Middle.tcl
-source StaticAnalysisEnv.tcl
-recorder Node -tcp 127.0.0.1 4114 -node 2 3 4 -dof 1 2 3 disp
-recorder Node -tcp 127.0.0.1 4115 -node 2 3 4 -dof 1 2 3 reaction
